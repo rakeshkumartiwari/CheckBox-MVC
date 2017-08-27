ed4b4889f9f000cc3404daabd1c6db88c627395e
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2017-08-27 19:45:21 +0530

    Checkbox in mvc

---
+++ MvcCheckbox/Controllers/DefaultController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCheckbox.Models;
+using System.Text;
+
+namespace MvcCheckbox.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = Class1.GetUser();
+            return View(model);
+        }
+
+        [HttpPost]
+        public string Index(Class1 obj)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            if (obj.IsMonday || obj.IsTuesday || obj.IsWednesday || obj.IsThrusday || obj.IsFriday || obj.IsSaturday || obj.IsSunday)
+            {
+                sb.Append("You selected - ");
+                if (obj.IsMonday)
+                    sb.Append("Monday" + ",");
+                if (obj.IsTuesday)
+                    sb.Append("Tuesday" + ",");
+                if (obj.IsWednesday)
+                    sb.Append("Wednesday" + ",");
+                if (obj.IsThrusday)
+                    sb.Append("Thrusday" + ",");
+                if (obj.IsFriday)
+                    sb.Append("Friday" + ",");
+                if (obj.IsSaturday)
+                    sb.Append("Saturday" + ",");
+                if (obj.IsSunday)
+                    sb.Append("Sunday" + ",");
+
+                sb.Remove(sb.ToString().LastIndexOf(","), 1);
+            }
+            else
+            {
+                sb.Append("You didn't select any day.");
+            }
+
+
+
+            return sb.ToString();
+        }
+    }
+
+}
---
+++ MvcCheckbox/Models/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCheckbox.Models
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsMonday { get; set; }
+        public bool IsTuesday { get; set; }
+        public bool IsWednesday { get; set; }
+        public bool IsThrusday { get; set; }
+        public bool IsFriday { get; set; }
+        public bool IsSaturday { get; set; }
+        public bool IsSunday { get; set; }
+
+        public static Class1 GetUser()
+        {
+            var user = new Class1
+            {
+                Id = 1,
+                Name = "Rakesh",
+                IsMonday = true,
+                IsThrusday = false,
+                IsWednesday = false,
+                IsTuesday = false,
+                IsFriday = true,
+                IsSaturday = false,
+                IsSunday = false
+            };
+            return user;
+        }
+
+
+    }
+}
+
+
